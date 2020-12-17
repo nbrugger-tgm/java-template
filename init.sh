47998b1040fb164f3e5d3f9fbd0cd4056f7448ce
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2020-12-18 00:04:40 +0100

    Fix SED commands

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-read -p "Package (eg. com.niton)                :" package
-read -p "Repository: (eg. nbrugger-tgm/react4j) :" repo
-read -p "Name: (eg. react4j)                    :" name
-sed -i '' "s|{{package}}|$package|" build.gradle settings.gradle README-TEMPLATE.md autogen-config.sh
-sed -i '' "s|{{repo}}|$repo|" build.gradle settings.gradle README-TEMPLATE.md autogen-config.sh
-sed -i '' "s|{{name}}|$name|" build.gradle settings.gradle README-TEMPLATE.md autogen-config.sh
+read -p "Package (eg. com.niton)                : " package
+read -p "Repository: (eg. nbrugger-tgm/react4j) : " repo
+read -p "Name: (eg. react4j)                    : " name
+sed -i "s|{{package}}|$package|" build.gradle settings.gradle README-TEMPLATE.md autogen-config.sh
+sed -i "s|{{repo}}|$repo|" build.gradle settings.gradle README-TEMPLATE.md autogen-config.sh
+sed -i "s|{{name}}|$name|" build.gradle settings.gradle README-TEMPLATE.md autogen-config.sh
 mv README.md TEMPLATE-SETUP.md > /dev/null
 mv README-TEMPLATE.md README.md > /dev/null
 
